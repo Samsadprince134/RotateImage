a3dd1119eeccb47b19b098670986b46e10b1e319
Md Samsad Alam <91345589+Samsadprince134@users.noreply.github.com>
2024-09-14 04:58:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -78,3 +78,12 @@ app.post("/api/upload", upload.single("image"), async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.get("/", (request, response) => {
+  return response.json({
+    success : true,
+    message : "Your Server is Up and Running Successfully !!........."
+  })
+  
+})
